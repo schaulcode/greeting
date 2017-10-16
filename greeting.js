16b098ee75c05514b257446c545d306c72bf1df0
Scholi <scholimimi@gmail.com>
2017-10-16 19:59:05 +0300

    Modified html an JS code that user must agree see the programm

--- greeting.js
+++ greeting.js
@@ -1,3 +1,6 @@
-var name = prompt("Enter your name");
-var lastname = prompt("Enter your lastname")
-alert("Hello " + name + lastname);
+document.getElementsByTagName('button').addEventListener('click', function () {
+  var name = prompt("Enter your name");
+  var lastname = prompt("Enter your lastname")
+  alert("Hello " + name + lastname);
+
+})
